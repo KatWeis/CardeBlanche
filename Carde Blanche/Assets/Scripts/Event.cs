a2dff4cb8cf242d32de1d1682c2221c048083db3
James Troup <jlt9768@ad.rit.edu>
2019-01-23 10:53:34 -0500

    Started work on event dictionary code

---
+++ Carde Blanche/Assets/Scripts/Event.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event {
+    int Id { get { return Id; } set { Id = value; } }
+    string Name { get { return Name; } set { Name = value; } }
+    string Desc { get { return Desc; } set { Desc = value; } }
+    string Affinity { get { return Affinity; } set { Affinity = value; } }
+    public Event(int id, string name, string description, string affinity)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Desc = description;
+        this.Affinity = affinity;
+    }
+ };
+
---
+++ Carde Blanche/Assets/Scripts/EventDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class EventDictionary : MonoBehaviour {
+    Dictionary<int, Event> events = new Dictionary<int, Event>();
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Run");
+        ReadFile("Assets/Externals/events.txt");
+        
+        Debug.Log(events[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void ReadFile(string filePath)
+    {
+        StreamReader reader = new StreamReader(filePath);
+        int id;
+        string name;
+        string description;
+        string affinity;
+        while (!reader.EndOfStream)
+        {
+            id = Convert.ToInt32(reader.ReadLine());
+            affinity = reader.ReadLine();
+            name = reader.ReadLine();
+            description = reader.ReadLine();
+            
+            events.Add(id, new Event(id, name, description,affinity));
+            reader.ReadLine();
+        }
+    }
+}
