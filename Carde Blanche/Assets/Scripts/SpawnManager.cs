0c61e4e185bd3f6d4225e3013f86476c169805ea
Matt Lienhard <mcl1601>
2019-01-25 14:55:17 -0500

    Added an option to randomize the chance a spawn point spawns a prop

--- Carde Blanche/Assets/Scripts/SpawnManager.cs
+++ Carde Blanche/Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,9 @@ public class SpawnManager : MonoBehaviour {
     private int objectCount;
 
     private GameObject[] spawnPoints;
+
+    [Range(0.01f, 1f)]
+    public float spawnChance;
 	// Use this for initialization
 	void Start () {
         objectCount = spawnList.Length;
@@ -16,10 +19,14 @@ public class SpawnManager : MonoBehaviour {
 
         for(int i = 0; i < spawnPoints.Length; i++)
         {
-            int rand = Random.Range(0, objectCount);
-            GameObject temp = (GameObject)Instantiate(spawnList[rand], spawnPoints[i].transform.position, Quaternion.identity);
-            temp.transform.LookAt(Vector3.zero);
-            temp.transform.Rotate(transform.right, -90);
+            float chance = Random.Range(0.01f, 1f);
+            if(chance <= spawnChance)
+            {
+                int rand = Random.Range(0, objectCount);
+                GameObject temp = (GameObject)Instantiate(spawnList[rand], spawnPoints[i].transform.position, Quaternion.identity);
+                temp.transform.LookAt(Vector3.zero);
+                temp.transform.Rotate(transform.right, -90);
+            }
         }
 	}
 }
