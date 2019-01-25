e211a096d35ecda108bc772c761e3ed567ece0ac
Matt Lienhard <mcl1601>
2019-01-25 14:47:07 -0500

    Added a spawning system so we can generate props on the world

---
+++ Carde Blanche/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public GameObject[] spawnList;
+    private int objectCount;
+
+    private GameObject[] spawnPoints;
+	// Use this for initialization
+	void Start () {
+        objectCount = spawnList.Length;
+
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            int rand = Random.Range(0, objectCount);
+            GameObject temp = (GameObject)Instantiate(spawnList[rand], spawnPoints[i].transform.position, Quaternion.identity);
+            temp.transform.LookAt(Vector3.zero);
+            temp.transform.Rotate(transform.right, -90);
+        }
+	}
+}
