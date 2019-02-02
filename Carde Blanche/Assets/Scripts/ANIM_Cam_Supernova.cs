7c51c60ebb0a88cce68c8e0e2cc1d8d313793e7c
mcl1601 <mcl1601@g.rit.edu>
2019-02-02 16:14:23 -0500

    Added supernova skyrim easter egg, earthquake, lazers (Now up to 6 total events), toggle for auto orbiting camera, and some other minor stuff

---
+++ Carde Blanche/Assets/Scripts/ANIM_Cam_Supernova.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANIM_Cam_Supernova : MonoBehaviour {
+    public Transform target;
+    public float timeToLook;
+
+    private float percent = 0f;
+    private float timer = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timer < timeToLook)
+        {
+            percent = timer / timeToLook;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), percent);
+            timer += Time.deltaTime;
+        }
+    }
+}
---
+++ Carde Blanche/Assets/Scripts/InitVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class InitVideo : MonoBehaviour {
+    public VideoPlayer player;
+    public RawImage image;
+
+    RenderTexture text;
+
+    void Start()
+    {
+        text = new RenderTexture((int)player.clip.width, (int)player.clip.height, 0);
+
+        player.targetTexture = text;
+        image.texture = text;
+    }
+}
