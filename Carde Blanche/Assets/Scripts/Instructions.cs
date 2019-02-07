fa698009e6da03ef4893a35ef55193746fe5d760
KatWeis <kxw4589@g.rit.edu>
2019-02-07 18:02:24 -0500

    Put in an instructions overlay into the game

--- Carde Blanche/Assets/Scripts/Instructions.cs
+++ Carde Blanche/Assets/Scripts/Instructions.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Instructions : MonoBehaviour {
 
+    private int currentInstruct;
+
 	// Use this for initialization
 	void Start () {
-		
+        currentInstruct = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0) || Input.anyKeyDown)
+        {
+            currentInstruct++;
+            transform.GetChild(currentInstruct - 1).gameObject.SetActive(false);
+
+            if (currentInstruct >= transform.childCount)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            
+            transform.GetChild(currentInstruct).gameObject.SetActive(true);
+            
+        }
+    }
 }
